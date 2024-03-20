8531016876959884cf57d1356d6a9b1e161ce65c
jatin1793 <jatinwakodikar@gmail.com>
2024-03-20 09:21:54 +0530

    done

--- config/env_config.js
+++ config/env_config.js
@@ -3,9 +3,7 @@ require('dotenv').config();
 const env_config = {
     jwt_secret : process.env.JWT_SECRET,
     jwt_token_expire : process.env.JWT_TOKEN_EXPIRE,
-
     mongodb_url : process.env.MONGODB_URL,
-    frontend_url: process.env.FRONTEND_URL,
 }
 
 module.exports = env_config;
